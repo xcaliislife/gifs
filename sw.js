cd76a2c013506f6f052178c82448540bb6655bff
xcaliislife <calix360x@gmail.com>
2026-03-16 18:22:20 -0600

    Fix formatting issues in sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const ASSETS = [
   './icon-192.png',
   './icon-512.png'
 ];
-
+``
 // Instalación y cacheo
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
